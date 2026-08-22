b682967a7f72a679b9e406bb742b009a0376f163
Jhonduran <john202196@gmail.com>
2026-08-21 20:55:46 -0500

    cambio de  ocupacion o servicios

--- Front-end/src/Pages/SearchService.jsx
+++ Front-end/src/Pages/SearchService.jsx
@@ -28,7 +28,7 @@ const mockConsultants = [
   {
     id: 2,
     name: 'jeimmy arguello .',
-    title: 'Business Intelligence Analyst',
+    title: 'psicóloga organizacional',
     image: usuario2,
     description: 'Consultora BI con enfoque en visualización de datos y dashboards interactivos. Especialista en PowerBI, Tableau y transformación de datos complejos en insights accionables.',
     availability: 'Disponible desde la próxima semana',
